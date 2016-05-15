8a146acb4b9bbe94f4579f0f1df11f13b977900d
Chuck Conway <charles@cconway.com>
2016-05-14 20:47:49 -0700

    added support for middleware at the route level

--- dist/index.js
+++ dist/index.js
@@ -100,7 +100,7 @@ function addRoutes(router, controller, action) {
 
     for (var i = 0; i < routes.length; i++) {
       var r = routes[i];
-      router[r.httpMethod.toLowerCase()](r.path, r.fn);
+      router[r.httpMethod.toLowerCase()](r.path, r.middleware, r.fn);
 
       console.log('Controller ' + controller.name + ' added method ' + r.httpMethod + ' for path ' + r.path);
     }
@@ -109,31 +109,31 @@ function addRoutes(router, controller, action) {
   }
 }
 
-function httpGet(path) {
+function httpGet(path, middleware) {
   return function (target, key, descriptor) {
-    route(target, key, descriptor, 'GET', path);
+    route(target, key, descriptor, 'GET', path, middleware);
   };
 }
 
-function httpPost(path) {
+function httpPost(path, middleware) {
   return function (target, key, descriptor) {
-    route(target, key, descriptor, 'POST', path);
+    route(target, key, descriptor, 'POST', path, middleware);
   };
 }
 
-function httpPut(path) {
+function httpPut(path, middleware) {
   return function (target, key, descriptor) {
-    route(target, key, descriptor, 'PUT', path);
+    route(target, key, descriptor, 'PUT', path, middleware);
   };
 }
 
-function httpDelete(path) {
+function httpDelete(path, middleware) {
   return function (target, key, descriptor) {
-    route(target, key, descriptor, 'DELETE', path);
+    route(target, key, descriptor, 'DELETE', path, middleware);
   };
 }
 
-function route(target, key, descriptor, httpMethod, path) {
+function route(target, key, descriptor, httpMethod, path, middleware) {
   var fn = descriptor.value;
 
   delete descriptor.value;
@@ -159,5 +159,5 @@ function route(target, key, descriptor, httpMethod, path) {
     target.routes = [];
   }
 
-  target.routes[target.routes.length] = { path: path, httpMethod: httpMethod, fn: fn };
+  target.routes[target.routes.length] = { path: path, httpMethod: httpMethod, fn: fn, middleware: middleware };
 };
